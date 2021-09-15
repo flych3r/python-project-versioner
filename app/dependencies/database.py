e7aaaf476906378b2f0e634fe9ae7bc9850bc0e1
flych3r <matheus.sampaio011@gmail.com>
2021-09-15 18:28:50 -0300

    :rocket: fix postgres url on migrations

--- app/dependencies/database.py
+++ app/dependencies/database.py
@@ -5,15 +5,15 @@ from sqlalchemy.orm import sessionmaker
 from app.dependencies.config import SETTINGS, AppEnv
 
 connect_args = {}
-db_url = SETTINGS.database_url
+database_url = SETTINGS.database_url
 
 if SETTINGS.app_env == AppEnv.test:
     connect_args = {'check_same_thread': False}
 else:
-    if db_url.startswith('postgres://'):
-        db_url = db_url.replace('postgres://', 'postgresql://', 1)
-print(db_url)
-engine = create_engine(db_url, connect_args=connect_args)
+    if database_url.startswith('postgres://'):
+        database_url = database_url.replace('postgres://', 'postgresql://', 1)
+
+engine = create_engine(database_url, connect_args=connect_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- migrations/env.py
+++ migrations/env.py
@@ -6,6 +6,8 @@ from sqlalchemy import engine_from_config, pool
 from app.dependencies.config import SETTINGS
 
 database_url = SETTINGS.database_url
+if database_url.startswith('postgres://'):
+    database_url = database_url.replace('postgres://', 'postgresql://', 1)
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
